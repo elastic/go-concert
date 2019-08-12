fe99d0e2af8c9ae6e6303649287ab664245cc2f8
Pier-Hugues Pellerin <phpellerin@gmail.com>
2019-08-12 08:59:27 -0400

    Add a Closer to synchronize parent and children go-routine (#7)
    * Add a Closer to synchronize parent and children go-routine
    
    Add a new Closer struct that implements a CloserRef interface, that
    interface is a subset of a context.Context and allow for easier control
    of go-routines that are start in a parent and children relationship.
    
    Example:
    
    You have a TCP server that spawn a new go-routines per clients and you
    want to close all the go-routines when you are shutting down the server.
    You create a Closer and you pass a sub closer, both closer will maintain
    a relationship, if you call close on the parent, the `Done()` function  of the
    children will unblock and allow you to shutdown gracefully.
    
    If you close a Child with the `Close()` the child will remove itself
    from the parent list of children making sure that Close() is only called
    once.

---
+++ chorus/closeref_test.go
@@ -0,0 +1,59 @@
+package chorus
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCloserRef(t *testing.T) {
+	testPropageToChilds(t)
+	testDontPropagateParent(t)
+	testReturnErrWhenAlreadyClosed(t)
+}
+
+func testPropageToChilds(t *testing.T) {
+	var parentCheck bool
+	var childCheck bool
+	parent := NewCloser(func() {
+		parentCheck = true
+	})
+
+	_ = WithCloser(parent, func() {
+		childCheck = true
+	})
+
+	parent.Close()
+	assert.True(t, parentCheck)
+	assert.True(t, childCheck)
+}
+
+func testDontPropagateParent(t *testing.T) {
+	var parentCheck bool
+	var childCheck bool
+	var c int
+	parent := NewCloser(func() {
+		parentCheck = true
+	})
+
+	child := WithCloser(parent, func() {
+		childCheck = true
+		c++
+	})
+
+	child.Close()
+	assert.False(t, parentCheck)
+	assert.True(t, childCheck)
+	assert.Equal(t, 1, c)
+
+	// Child remove itself from the chain
+	parent.Close()
+	assert.Equal(t, 1, c)
+}
+
+func testReturnErrWhenAlreadyClosed(t *testing.T) {
+	parent := NewCloser(nil)
+	assert.Nil(t, parent.Err())
+	parent.Close()
+	assert.Equal(t, ErrClosed, parent.Err())
+}
