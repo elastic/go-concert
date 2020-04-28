7d90de73ea9a3015584e7e276424194057d75801
urso <steffen.siering@elastic.co>
2020-04-28 14:43:51 +0200

    fix import

--- unison/safewaitgroup_test.go
+++ unison/safewaitgroup_test.go
@@ -3,8 +3,8 @@ package unison
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gotest.tools/assert"
 )
 
 func TestSafeWaitGroup(t *testing.T) {
