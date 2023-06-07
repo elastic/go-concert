dc23afc4ba19c7a1829cd0b7a62de7adba17ffaf
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-06-07 16:08:39 +0200

    add scripts

---
+++ .buildkite/scripts/test.ps1
@@ -0,0 +1,39 @@
+$ErrorActionPreference = "Stop" # set -e
+# Forcing to checkout again all the files with a correct autocrlf.
+# Doing this here because we cannot set git clone options before.
+function fixCRLF {
+    Write-Host "-- Fixing CRLF in git checkout --"
+    git config core.autocrlf input
+    git rm --quiet --cached -r .
+    git reset --quiet --hard
+}
+
+function withGolang($version) {
+    Write-Host "-- Install golang --"
+    choco install -y golang --version $version
+    $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+    Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+    refreshenv
+    go version
+    go env
+}
+
+function withGoJUnitReport {
+    Write-Host "-- Install go-junit-report --"
+    go install github.com/jstemmer/go-junit-report/v2@latest
+}
+
+# Prepare enviroment
+fixCRLF
+withGolang $env:SETUP_GOLANG_VERSION
+withGoJUnitReport
+
+# Run test
+$ErrorActionPreference = "Continue" # set +e
+mkdir -p build
+SET OUT_FILE=build\output-report.out
+go test "./..." -v > $OUT_FILE | type $OUT_FILE
+$EXITCODE=$LASTEXITCODE
+$ErrorActionPreference = "Stop"
+
+go-junit-report > build\junit-$GO_VERSION.xml < $OUT_FILE
