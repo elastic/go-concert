574fa464be6a4254c46cc540d473ab9e47e3384a
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-06-07 16:08:39 +0200

    add scripts

---
+++ .buildkite/scripts/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Prepare enviroment
+source .buildkite/scripts/pre-install-command.sh
+add_bin_path
+with_go_junit_report
+
+# Run the tests
+set +e
+export OUT_FILE="build/test-report.out"
+mkdir -p build
+go test "./..." -v 2>&1 | tee ${OUT_FILE}
+status=$?
+
+go get -v -u github.com/jstemmer/go-junit-report
+go-junit-report > "build/junit-${GO_VERSION}.xml" < ${OUT_FILE}
+
+exit ${status}
