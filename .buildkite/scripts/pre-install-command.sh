7f43bcca619042da234e15cb56581068daac4751
Svetlana Maltseva <58.svetlana.maltseva@gmail.com>
2023-11-08 11:46:55 +0100

    BK: Add orka-mac for testing (#66)
    Added a new steps for testing on macOS environment since we didn't have it in the previous iteration
    
    Windows tests are randomly failing (_TestRetryUntil/retryuntil_returns_deadline_exceeded_error_) but they aren't related to this commit.

--- .buildkite/scripts/pre-install-command.sh
+++ .buildkite/scripts/pre-install-command.sh
@@ -12,4 +12,29 @@ with_go_junit_report() {
     go install github.com/jstemmer/go-junit-report/v2@latest
 }
 
+with_go() {
+    go_version=$1
+    url=$(get_gvm_link "${GVM}")
+    WORKSPACE=${WORKSPACE:-"$(pwd)"}
+    mkdir -p "${WORKSPACE}/bin"
+    export PATH="${PATH}:${WORKSPACE}/bin"
+    retry 5 curl -L -o "${WORKSPACE}/bin/gvm" "${url}"
+    chmod +x "${WORKSPACE}/bin/gvm"
+    ls ${WORKSPACE}/bin/
+    eval "$(gvm $go_version)"
+    go_path="$(go env GOPATH):$(go env GOPATH)/bin"
+    export PATH="${PATH}:${go_path}"
+    go version
+}
+
+# for gvm link
+get_gvm_link() {
+    gvm_version=$1
+    platform_type="$(uname)"
+    arch_type="$(uname -m)"
+    [[ ${arch_type} == "aarch64" ]] && arch_type="arm64" # gvm do not have 'aarch64' name for archetecture type
+    [[ ${arch_type} == "x86_64" ]] && arch_type="amd64"
+    echo "https://github.com/andrewkroh/gvm/releases/download/${gvm_version}/gvm-${platform_type}-${arch_type}"
+}
+
 WORKSPACE=${WORKSPACE:-"$(pwd)"}
